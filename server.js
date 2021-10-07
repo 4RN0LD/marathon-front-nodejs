0cbfa454bacfc5e4fc2bf137660b095f48dddf2c
4RN0LD <arnold.norabuena@gmail.com>
2021-10-07 02:35:37 -0500

    corrigiendo puerto

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const server = express();
 
 server.use("/", express.static("marathon-front"));
 
-server.listen(3000, () => {
-    console.log('front en el puerto 3000');
+server.listen(5000, () => {
+    console.log('front en el puerto 5000');
 });
