7cbd79b04b37af4eac72c59b71ccad533bbfe8d6
4RN0LD <arnold.norabuena@gmail.com>
2021-10-07 02:40:28 -0500

    moviendo al puerto 8080

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const server = express();
 
 server.use("/", express.static("marathon-front"));
 
-server.listen(5000, () => {
-    console.log('front en el puerto 5000');
+server.listen(8080, () => {
+    console.log('front en el puerto 8080');
 });
